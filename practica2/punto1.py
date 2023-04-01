1657411fa07efb909cacaceb3510c4658227b91d
Franco <francogrch@gmail.com>
2023-04-01 03:10:06 -0300

    casi_termiando

--- practica2/punto1.py
+++ practica2/punto1.py
@@ -214,15 +214,15 @@ print(f"""#Palabra: {word}
 #valor: {sum_points}""")
 
 print(f"{'Punto 10':-^40}")
-nombres = ''' 'Agustin', 'Alan', 'Andrés', 'Ariadna', 'Bautista', 'CAROLINA', 'CESAR', 'David','Diego', 'Dolores', 'DYLAN', 'ELIANA', 'Emanuel', 'Fabián', 'Facundo', 'Francsica', 'FEDERICO', 'Fernanda', 'GONZALO', 'Gregorio', 'Ignacio', 'Jonathan', 'Joaquina', 'Jorge','JOSE', 'Javier', 'Joaquín' , 'Julian', 'Julieta', 'Luciana', 'LAUTARO', 'Leonel', 'Luisa', 'Luis', 'Marcos', 'María', 'MATEO', 'Matias', 'Nicolás', 'Nancy', 'Noelia', 'Pablo', 'Priscila', 'Sabrina', 'Tomás', 'Ulises', 'Yanina' '''
-notas_1 = [81, 60, 72, 24, 15, 91, 12, 70, 29, 42, 16, 3, 35, 67, 10, 57, 11, 69,
-12, 77, 13, 86, 48, 65, 51, 41, 87, 43, 10, 87, 91, 15, 44,
-85, 73, 37, 42, 95, 18, 7, 74, 60, 9, 65, 93, 63, 74]
-notas_2 = [30, 95, 28, 84, 84, 43, 66, 51, 4, 11, 58, 10, 13, 34, 96, 71, 86, 37,
-64, 13, 8, 87, 14, 14, 49, 27, 55, 69, 77, 59, 57, 40, 96, 24, 30, 73,
-95, 19, 47, 15, 31, 39, 15, 74, 33, 57, 10]
+
+
 #Parte A
-def generarDic():
+def generarDic(nombres,notas_1,notas_2):
+    """ Esta funcion genera un diccionario con nombres como llaves, y dos notas por nombre, como valor.
+    Recibiendo como parametro: 
+    -un string donde estan todos las keys separadas por , y espacio
+    -dos listas con valores donde el indice es el mismo que la posicion de su key en el string.
+    """
     nombres_lista = nombres.replace("'","").replace(",","").split()
     alum_dic = {}
     for indice, elem in enumerate(nombres_lista):
@@ -230,12 +230,19 @@ def generarDic():
     return alum_dic
 #Parte B
 def est_prom(alum_dic):
+    """ Esta funcion genera un diccionario con nombres como llaves y con el promedio de las dos notas como valor.
+    Recibe como parametro:
+    -un diccionario con srtrings como llaves, y como valor una lista con dos enteros.
+    """
     alum_prom = []
     for key in alum_dic.keys():
         alum_prom[key] = (alum_dic[key][0] + alum_dic[key][1]) / 2
     return alum_prom
 #Parte C
-def cur_prom(alum_prom)
+def cur_prom(alum_prom):
+    """ Esta funcion retorna el promedio general de de los valores de un diccionario. Recibe como parametro un 
+    diccionario con valores de tipo enteros.
+    """
     prom_general = 0
     for key in alum_prom.keys():
         prom_general += alum_prom[key]
@@ -243,8 +250,21 @@ def cur_prom(alum_prom)
     return prom_general
 #Parte D
 def estu_prom_max(alum_prom):
+    """Esta funcion retorna la llave con el maximo valor en un diccionario, recibe como parametro un diccionario
+    con valores de tipo enteros.
+    """
     return max(alum_prom)
 
 #Parte E
 def est_nota_min(alum_dic):
+    """Esta funcion retorna la llave con el minimo valor en un diccionario
+    """
     return min(alum_dic)
+
+nombres = ''' 'Agustin', 'Alan', 'Andrés', 'Ariadna', 'Bautista', 'CAROLINA', 'CESAR', 'David','Diego', 'Dolores', 'DYLAN', 'ELIANA', 'Emanuel', 'Fabián', 'Facundo', 'Francsica', 'FEDERICO', 'Fernanda', 'GONZALO', 'Gregorio', 'Ignacio', 'Jonathan', 'Joaquina', 'Jorge','JOSE', 'Javier', 'Joaquín' , 'Julian', 'Julieta', 'Luciana', 'LAUTARO', 'Leonel', 'Luisa', 'Luis', 'Marcos', 'María', 'MATEO', 'Matias', 'Nicolás', 'Nancy', 'Noelia', 'Pablo', 'Priscila', 'Sabrina', 'Tomás', 'Ulises', 'Yanina' '''
+notas_1 = [81, 60, 72, 24, 15, 91, 12, 70, 29, 42, 16, 3, 35, 67, 10, 57, 11, 69,
+12, 77, 13, 86, 48, 65, 51, 41, 87, 43, 10, 87, 91, 15, 44,
+85, 73, 37, 42, 95, 18, 7, 74, 60, 9, 65, 93, 63, 74]
+notas_2 = [30, 95, 28, 84, 84, 43, 66, 51, 4, 11, 58, 10, 13, 34, 96, 71, 86, 37,
+64, 13, 8, 87, 14, 14, 49, 27, 55, 69, 77, 59, 57, 40, 96, 24, 30, 73,
+95, 19, 47, 15, 31, 39, 15, 74, 33, 57, 10]
